6e8bb8e350ea88ba30d95a4d19e6e6699e86ca85
Miguel2236 <miguelm2236@gmail.com>
2020-10-30 18:31:05 -0600

    migraciones para las nuevas tablas

---
+++ database/migrations/2020_10_30_235704_create_permissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('section_id')->constrained('sections')->onDelete('cascade');
+            $table->string('name',100);
+            $table->string('identify',100);
+            $table->string('button',100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+    }
+}
--- database/migrations/2020_10_31_000806_create_profiles_permissions_table.php
+++ database/migrations/2020_10_31_000806_create_profiles_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProfilesPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles_permissions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('departament_id')->constrained('departaments')->onDelete('cascade');
             $table->foreignId('profile_id')->constrained('profiles')->onDelete('cascade');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('permission_id')->constrained('permissions')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profiles_permissions');
     }
 }
